4b81121727db022a30fc31444894246afebd9110
Amanda Hogan <ahogan@teamworks.com>
2020-03-18 13:49:19 -0400

    Find and find by id methods

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -5,30 +5,40 @@ module.exports = {
   findById,
   findSteps,
   add,
+  addStep,
   update,
   remove
 };
 
 function find() {
-
+  return db('schemes');
 };
 
 function findById(id) {
-
+  return db('schemes').where({ id }).first();
 };
 
+// Write
 function findSteps(id) {
-
+  return
 };
 
+// Fix to return full object
 function add(scheme) {
-
+  return db('schemes').insert(scheme);
 };
 
+// Write
+function addStep(step, scheme_id) {
+  return
+};
+
+// Fix to return updated object
 function update(changes, id) {
-
+  return db('schemes').where({ id }).update(changes);
 };
 
+// Fix to return deleted object
 function remove(id) {
-
+  return db('schemes').where({ id }).del();
 };
